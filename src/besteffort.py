56ee85bc9c5fdd6f828ebe93679acb47ea8bed20
Ethan Havemann <echavemann@gmail.com>
2022-07-26 21:00:09 -0500

    Update besteffort.py

--- src/besteffort.py
+++ src/besteffort.py
@@ -14,4 +14,17 @@ def print_all_raw_data():
                 csv_writer.writerow(raw_data)
             #raw data is a numpy array...
             print(read.read_id, raw_data)
+
+def read_file(file):
+    path = str(file)
+    with get_fast5_file(path, mode="r") as f5:
+        for read in f5.get_reads():
+            raw_data = read.get_raw_data()
+            raw_data = raw_data.flatten()
+            with open("newfile.csv", 'w', newline = '') as outfile:
+                csv_writer = csv.writer(outfile, delimiter = ',')
+                csv_writer.writerow(raw_data)
+            print(read.read_id, raw_data)
+
+        
 print_all_raw_data()
