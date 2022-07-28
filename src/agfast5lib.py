385662a6de233d8f4ed5fa8c148fef95ece4728b
Ethan Havemann <echavemann@gmail.com>
2022-07-28 14:47:50 -0500

    refactor, testing updates

--- src/agfast5lib.py
+++ src/agfast5lib.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import os
 
+#Testing Function
 def print_all_raw_data():
     fast5_filepath = 'src\\batch22.fast5' # This can be a single- or multi-read file
     with get_fast5_file(fast5_filepath, mode="r") as f5:
@@ -15,13 +16,14 @@ def print_all_raw_data():
             #raw data is a numpy array...
             print(read.read_id, raw_data)
 
+#REQUIRES THAT THE FILE INPUT IS THE SAME AS THE PATH!
 def read_file(file):
     path = str(file)
     with get_fast5_file(path, mode="r") as f5:
         for read in f5.get_reads():
             raw_data = read.get_raw_data()
             raw_data = raw_data.flatten()
-            with open("newfile.csv", 'w', newline = '') as outfile:
+            with open( path+'.csv', 'w', newline = '') as outfile:
                 csv_writer = csv.writer(outfile, delimiter = ',')
                 csv_writer.writerow(raw_data)
             print(read.read_id, raw_data)
